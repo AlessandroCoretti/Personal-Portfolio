38df871d987e1e2eb6e79261111e94f3bad4112d
Alessandro Coretti <alessandrocoretti2002@gmail.com>
2025-04-18 10:30:08 +0200

    capstone-pause

--- src/components/right/Projects.jsx
+++ src/components/right/Projects.jsx
@@ -1,5 +1,7 @@
 import EpiBook from "./projects/EpiBook";
-import Freeitinerary from "./projects/Freeitinerary";
+{
+  /*import Freeitinerary from "./projects/Freeitinerary";*/
+}
 import MeteoApp from "./projects/MeteoApp";
 import Netflix from "./projects/Netflix";
 
@@ -7,7 +9,7 @@ function Projects() {
   return (
     <section id="projects">
       <div className="flex flex-col mb-10 lg:mb-0">
-        <Freeitinerary />
+        {/*<Freeitinerary />*/}
         <Netflix />
         <MeteoApp />
         <EpiBook />
