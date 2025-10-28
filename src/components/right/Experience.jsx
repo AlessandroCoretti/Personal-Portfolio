3ff5a154e000d13873ac9de84d96dd4c5065688e
Alessandro Coretti <alessandrocoretti2002@gmail.com>
2025-10-28 17:24:53 +0100

    experience added

--- src/components/right/Experience.jsx
+++ src/components/right/Experience.jsx
@@ -45,6 +45,57 @@ function Experience() {
             </div>
           </div>
         </li>
+        <li className="mb-12">
+          <div className="grid grid-cols-8  !items-top hover:bg-slate-800/50 hover:p-2 hover:rounded hover:scale-[1.05] transition-all duration-300 easein-out ">
+            <div className="col-span-8 lg:col-span-2 mb-3 lg:mb-0">
+              <header className="uppercase !text-slate-300 text-xs font-bold mt-1">mag 2025- ott 2025</header>
+            </div>
+            <div className="col-span-8 lg:col-span-6">
+              <div className="flex items-center text-slate-200 font-bold text-lg gap-2 hover:text-emerald-300 duration-200 ese-in">
+                <h3>Content Editor</h3>
+                <span className="hidden lg:block"> - </span>
+                <div>PUBLICIS SAPIENT</div>
+                <a href="https://www.publicissapient.com/" target="_blank" rel="noopener noreferrer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="#E2E8F0"
+                    className="bi bi-box-arrow-up-right hover:fill-emerald-300 duration-200 ese-in"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
+                    />
+                  </svg>
+                </a>
+              </div>
+              <section className="text-slate-400 text-md lg:text-sm font-medium mt-2">
+                <p>
+                  Durante la mia esperienza in Publicis Sapient, ho collaborato da remoto alla gestione e all’ottimizzazione dei siti web globali di{" "}
+                  <a href="https://www.maserati.com/it/it">
+                    <strong className="transition duration-150 ease-in-out hover:text-white">Maserati</strong>
+                  </a>
+                  , utilizzando Adobe CMS (AEM) per garantire coerenza e qualità dei contenuti su scala internazionale. Inoltre, ho partecipato attivamente al
+                  rilascio della collezione estiva di{" "}
+                  <a href="https://www.maserati.com/it/it/modelli/mcpura">
+                    <strong className="transition duration-150 ease-in-out hover:text-white">McPura</strong>
+                  </a>{" "}
+                  e{" "}
+                  <a href="https://www.maserati.com/it/it/modelli/mcpura-cielo">
+                    <strong className="transition duration-150 ease-in-out hover:text-white">McPura Cielo</strong>
+                  </a>
+                  , supportando le attività di pubblicazione e aggiornamento dei contenuti digitali.
+                </p>
+              </section>
+            </div>
+          </div>
+        </li>
       </ol>
       <footer>
         <a
