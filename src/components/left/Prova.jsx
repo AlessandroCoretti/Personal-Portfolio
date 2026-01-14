5915bdd7938406be48ffb6ad605e502916e023f8
Alessandro Coretti <alessandrocoretti2002@gmail.com>
2026-01-14 10:52:06 +0100

    prova 5

--- src/components/left/Prova.jsx
+++ src/components/left/Prova.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function OpenInBrowserButton() {
   const handleClick = () => {
     // Apri una pagina di redirect intermedia
-    window.location.href = "https://www.alessandrocoretti.com/redirect";
+    window.location.href = "https://www.alessandrocoretti.com";
   };
 
   return (
